26fbe50a0e6f4bc6e21696a50aff20b70d6fc05d
TakahashiEn <takahashien1999@gmail.com>
2020-10-18 15:12:53 +0800

    First WDM Code

---
+++ FirstWDM/MyDriver1/MyDriver1/main.c
@@ -0,0 +1,32 @@
+#include <ntddk.h>
+
+VOID doNothing(
+	HANDLE ppid,
+	HANDLE mypid,
+	BOOLEAN bcreate
+) {
+
+	DbgPrint("Process Notifying !!!\n");
+
+}
+
+VOID DriverUnload(
+	PDRIVER_OBJECT pd
+) {
+	DbgPrint("Unload\n");
+	PsSetCreateProcessNotifyRoutine(doNothing, TRUE);
+}
+
+
+NTSTATUS DriverEntry(
+	PDRIVER_OBJECT driver,
+	PUNICODE_STRING regPath
+) {
+	driver->DriverUnload = DriverUnload;
+
+	DbgPrint("---%wZ---\n", regPath);
+
+	PsSetCreateProcessNotifyRoutine(doNothing, FALSE);
+
+	return 0;
+}
